744f65ec02df2582a89b3c60de0d59b262421173
Edisson Flores <flores96@gmail.com>
2017-03-20 15:16:52 -0400

    Quick Commit

---
+++ UrbanLauncher/MainMenuScene.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenuScene.swift
+//  UrbanLauncher
+//
+//  Created by Tech on 2017-03-20.
+//  Copyright © 2017 Adrian&Edisson. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class MainMenuScene: SKScene {
+    
+    
+    override func didMove(to view: SKView)
+    {
+        
+    }
+}
